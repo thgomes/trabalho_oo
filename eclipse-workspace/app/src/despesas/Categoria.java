f642109b8483d0cc87069497b65f12822d287367
sofia brasil <sofiabrasil44@gmail.com>
2020-10-20 13:27:19 -0300

    classe Categoria incompleta

---
+++ eclipse-workspace/app/src/despesas/Categoria.java
@@ -0,0 +1,24 @@
+package despesas;
+
+public class Categoria {
+	
+	private String descricao;
+	private float valor;
+	
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}public float getValor() {
+		return valor;
+	}public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+	
+	
+}
