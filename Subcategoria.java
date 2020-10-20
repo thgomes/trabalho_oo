27328c43cc7f9635c72c6161e2554f73070e3be1
GuilhermeBritoDeSouza <70172633+GuilhermeBritoDeSouza@users.noreply.github.com>
2020-10-20 14:37:34 -0300

    Add files via upload

---
+++ Subcategoria.java
@@ -0,0 +1,31 @@
+package despesas;
+
+public class Subcategoria extends Categoria{
+	
+	
+
+	@Override
+	public void setDescricao(String descricao) {
+		// TODO Auto-generated method stub
+		super.setDescricao(descricao);
+	}
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return super.getDescricao();
+	}
+	@Override
+	public void setValor(float valor) {
+		// TODO Auto-generated method stub
+		super.setValor(valor);
+	}
+	@Override
+	public float getValor() {
+		// TODO Auto-generated method stub
+		return super.getValor();
+	}
+	public Subcategoria() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
