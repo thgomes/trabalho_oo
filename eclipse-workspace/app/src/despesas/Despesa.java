303eba24f0683777a5d9e2044a727016aafdedcd
GuilhermeBritoDeSouza <70172633+GuilhermeBritoDeSouza@users.noreply.github.com>
2020-10-20 14:39:58 -0300

    Add files via upload

---
+++ eclipse-workspace/app/src/despesas/Despesa.java
@@ -0,0 +1,38 @@
+package despesas;
+
+public class Despesa {
+	protected String tipo;
+	protected float valordespesa;
+	protected float valordividido;
+
+	
+	public String getTipo() {
+		return tipo;
+	}public float getValor() {
+		return valordespesa;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}public void setValor(float valor) {
+		this.valordespesa = valor;
+	}
+	
+	public float igualitaria() {
+		
+		
+		
+		return valordividido;
+	}
+	
+	public float proporcional() {
+		
+		
+		
+		return valordividido;
+		
+		
+	}
+	
+	
+
+}
