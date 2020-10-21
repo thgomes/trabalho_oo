e486b06dee3644e2e278a8c441a501e1cc8365b0
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-10-21 00:37:55 -0300

    feature: add save method to class Despesa

--- eclipse-workspace/app/src/despesas/Despesa.java
+++ eclipse-workspace/app/src/despesas/Despesa.java
@@ -1,38 +1,52 @@
 package despesas;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class Despesa {
-	protected String tipo;
-	protected float valordespesa;
-	protected float valordividido;
+    private String descricao;
+    private Categoria categoria;
+    private Double valor;
 
-	
-	public String getTipo() {
-		return tipo;
-	}public float getValor() {
-		return valordespesa;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}public void setValor(float valor) {
-		this.valordespesa = valor;
-	}
-	
-	public float igualitaria() {
-		
-		
-		
-		return valordividido;
-	}
-	
-	public float proporcional() {
-		
-		
-		
-		return valordividido;
-		
-		
-	}
-	
-	
+    public Despesa(String descricao, Categoria categoria, Double valor) {
+        this.setDescricao(descricao);
+        this.setCategoria(categoria);
+        this.setValor(valor);
+    }
 
+	public String getDescricao() {
+		return this.descricao;
+    }
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+    public Double getValor() {
+        return this.valor;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void salvar() {
+        try {
+            FileWriter fw = new FileWriter("./despesas_10_20.txt", true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(this.descricao + ";" + this.categoria.getDescricao() + ";" + this.categoria.getSubcategoria().getDescricao() + ";" + this.valor);
+            pw.close();
+            fw.close();
+
+            System.out.println("\nDespesa cadastrada");
+
+        } catch(IOException err) {
+            System.out.println("\nErro de cadastro" + err);
+        }
+        
+    }
 }
