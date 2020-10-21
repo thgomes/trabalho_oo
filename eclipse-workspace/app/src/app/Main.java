e9ff3414166bb317575412c0f6acdf615b9ab8be
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-10-21 00:47:45 -0300

    refactoring: replace the Republica class to a Main class

---
+++ eclipse-workspace/app/src/app/Main.java
@@ -0,0 +1,62 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.IOException;
+
+import pessoas.Pessoa;
+
+import despesas.Despesa;
+import despesas.Categoria;
+import despesas.Subcategoria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int operacao;
+        operacao = 0;
+        
+        while (operacao != 5) {
+
+            System.out.println("\nDigite o número da operação desejada\n");
+
+            System.out.println("1 - Cadastrar uma pessoa");
+            System.out.println("2 - Cadastrar uma despesa");
+            System.out.println("3 - Calcular valor a ser pago igualitariamente");
+            System.out.println("4 - Calcular valor a ser pago proporcionalmente");
+            System.out.println("5 - Sair");
+
+
+            operacao = in.nextInt();
+
+            if (operacao == 1) {
+                cadastrarPessoa();
+
+            } else if (operacao == 2) {
+                cadastrarDespesa();
+
+            } else if (operacao == 3) {
+                calcularIgualitariamente();
+
+            } else if (operacao == 4) {
+                contarAlunos();
+            }
+
+        }
+ 
+    } 
+
+
+
+}
+
+	
+	
+
+	
+
+
+
