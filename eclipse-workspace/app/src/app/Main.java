ee235745551e3505d032347a1b403001c3e81585
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-12-06 16:16:59 -0300

    Adiciona try catch e mensagens de excecao na Main

--- eclipse-workspace/app/src/app/Main.java
+++ eclipse-workspace/app/src/app/Main.java
@@ -1,95 +1,81 @@
 package app;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Scanner;
-import java.io.IOException;
-
-import app.pessoas.Pessoa;
-
-import app.despesas.Despesa;
-import app.despesas.Categoria;
-import app.despesas.Subcategoria;
+import javax.swing.JOptionPane;
+import app.cadastros.Cadastro;
+import app.relatorios.Relatorio;
+import app.excecoes.*;
 
 public class Main {
+    public static void main(String[] args)  {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Cadastro cadastro = new Cadastro();
+        Relatorio relatorio = new Relatorio();
         int operacao;
-        operacao = 0;
-        
-        while (operacao != 5) {
-
-            System.out.println("\nDigite o número da operação desejada\n");
-
-            System.out.println("1 - Cadastrar uma pessoa");
-            System.out.println("2 - Cadastrar uma despesa");
-            System.out.println("3 - Calcular valor a ser pago igualitariamente");
-            System.out.println("4 - Calcular valor a ser pago proporcionalmente");
-            System.out.println("5 - Sair");
-
-
-            operacao = in.nextInt();
+		operacao = 0;
+		
+        while (operacao != 6) {
+        	
+            operacao=Integer.parseInt(JOptionPane.showInputDialog("Escolha o n�mero da op��o desejada: \n"
+            		+ "1 - Cadastrar uma pessoa\n"
+            		+"2 - Cadastrar uma despesa\n"
+            		+"3 - Calcular valor a ser pago igualitariamente\n"
+            		+"4 - Calcular valor a ser pago proporcionalmente\n"
+            		+"5 - Para informar todas as pessoas cadastradas e as despesas\n"
+            		+"6 - Sair\n**************************************************************"));
 
             if (operacao == 1) {
-                cadastrarPessoa();
-
-            } else if (operacao == 2) {
-                cadastrarDespesa();
-
+					try {
+						cadastro.cadastraPessoa();
+					} catch (RendimentoInvalidoException e) {
+						String msg= "exce��o capturada, cadastre esse morador(a) novamente digitando 1 no menu\n";
+						msg+=e.getMessage()+"\n";
+						JOptionPane.showMessageDialog(null,msg);
+						e.printStackTrace();
+					}
+					catch(DadosPessoaisIncompletosException e) {
+						String msg= "exce��o capturada!\nNome, email ou rendimento n�o informados, cadastre esse morador(a) novamente digitando 1 no menu\n";
+						msg+=e.getMessage()+"\n";
+						JOptionPane.showMessageDialog(null,msg);
+						e.printStackTrace();
+					}
+            } else if (operacao == 2) {      	
+            	try {
+					try {
+						cadastro.cadastraDespesa();
+					} catch (ValorNaoInformadoException e) {
+						String msg= "exce��o capturada!\nValor n�o informado, cadastre essa despesa novamente digitando 2 no menu\n";
+						msg+=e.getMessage()+"\n";
+						JOptionPane.showMessageDialog(null,msg);
+						e.printStackTrace();
+						e.printStackTrace();
+					}
+					} catch (CategoriaNaoInformadaException e) {
+					
+					String msg= "exce��o capturada!\nCategoria n�o informada, cadastre essa despesa novamente digitando 2 no menu\n";
+					msg+=e.getMessage()+"\n";
+					JOptionPane.showMessageDialog(null,msg);
+					e.printStackTrace();
+					} catch (DescricaoNaoInformadaException e) {
+					String msg= "exce��o capturada!\nDescri��o de despesa n�o informada, cadastre essa despesa novamente digitando 2 no menu\n";
+					msg+=e.getMessage()+"\n";
+					JOptionPane.showMessageDialog(null,msg);
+					e.printStackTrace();
+					
+					e.printStackTrace();
+					}
             } else if (operacao == 3) {
-                calcularIgualitariamente();
+                relatorio.calculoIgualitario();
+                
 
             } else if (operacao == 4) {
-                contarAlunos();
+                relatorio.calculoProporcional();
             }
-
+            else if(operacao==5) {
+            	cadastro.imprimirPessoa();
+            	cadastro.imprimirCadastro();            	
+            }
         }
- 
     } 
-
-		public static void cadastrarPessoa() {
-        Scanner in = new Scanner(System.in);
-        String nome, email;
-        double rendimento;
-
-        System.out.println("\nNome: ");
-        nome = in.nextLine();
-
-        System.out.println("\nEmail: ");
-        email = in.nextLine();
-
-        System.out.println("\nRendimento: ");
-        rendimento = in.nextDouble();
-        
-        Pessoa novaPessoa = new Pessoa(nome, email, rendimento);
-        
-        novaPessoa.salvar();
-    }
-
-		public static void cadastrarDespesa() {
-        Scanner in = new Scanner(System.in);
-        String descricaoDespesa, descricaoCategoria, descricaoSubcategoria;
-        double valor;
-
-        System.out.println("\nDescrição: ");
-        descricaoDespesa = in.nextLine();
-
-        System.out.println("\nCategoria: ");
-        descricaoCategoria = in.nextLine();
-
-        System.out.println("\nSubcategoria: ");
-        descricaoSubcategoria = in.nextLine();
-
-        System.out.println("\nValor: ");
-        valor = in.nextDouble();
-
-        Subcategoria novaSubcategoria = new Subcategoria(descricaoSubcategoria);
-        Categoria novaCategoria = new Categoria(descricaoCategoria, novaSubcategoria);
-        Despesa novaDespesa = new Despesa(descricaoDespesa, novaCategoria, valor);
-        
-        novaDespesa.salvar();
-    }
 }
 
 
